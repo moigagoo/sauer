e184e34facdd1660b517e428c3853ed37a759672
Constantine Molchanov <moigagoo@duck.com>
2024-08-31 09:08:41 +0400

    Bump version.

--- src/sauer/commands/sauer.nim
+++ src/sauer/commands/sauer.nim
@@ -12,7 +12,7 @@ import ../templates/app/pages/page
 const
   helpText = """Welcome to Sauer!
 
-Version: 1.0.8
+Version: 1.1.0
 
 Available commands:
     sauer init                initialize new Sauer project in the current directory
